9ac8fdd19ebc1437d12fd2304da6f98e3900876e
micaela benitez <mabenittez@gmail.com>
2026-02-12 18:14:39 -0300

    add cursos table migration and jpa entity mapping

--- src/main/java/com/mb/foro_hub/domain/curso/Curso.java
+++ src/main/java/com/mb/foro_hub/domain/curso/Curso.java
@@ -1,6 +1,20 @@
 package com.mb.foro_hub.domain.curso;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode(of = "id")
+@Table(name = "cursos")
+@Entity
 public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String categoria;
